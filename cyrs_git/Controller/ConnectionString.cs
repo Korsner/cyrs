8ceeba687df32f2881107d7bd42099e4332fcff5
BDS <d.borodachev@mail.ru>
2021-11-06 16:08:04 +0700

    Добавьте файлы проекта.

---
+++ cyrs_git/Controller/ConnectionString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cyrs_git.Controller
+{
+    class ConnectionString
+    {
+        public static string ConnStr
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["Sample.Properties.Settings.ConnStr"].ConnectionString;
+            }
+        }
+    }
+}
