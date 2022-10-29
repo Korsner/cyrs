9ac9cc9d1b8d49c23b24a8d4a2be3d4e1fd92a9c
Korsner <57417233+Korsner@users.noreply.github.com>
2022-10-29 19:52:03 +0700

    Работа с БД в отдельном проекте (#6)

---
+++ DbLibrary/Query.cs
@@ -0,0 +1,198 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+namespace DbLibrary.Controller
+{
+    public static class SQL
+    {
+        public static string ReadData = @"
+SELECT V_FIRM.TLGR AS Фирма, VIDTC.SHNAME AS [Вид ТС], TIPTR.TNAME AS [Тип ТС], PTS.UNTS AS [Учетный номер ТС] 
+FROM(
+    (V_FIRM INNER JOIN PTS ON V_FIRM.FIRMID = PTS.FIRMID) 
+    INNER JOIN TIPTR ON TIPTR.TID = PTS.TID
+) 
+INNER JOIN VIDTC ON VIDTC.VIDT = TIPTR.VIDT 
+GROUP BY V_FIRM.TLGR, VIDTC.SHNAME, TIPTR.TNAME, PTS.UNTS;";
+
+        public static string ReadDetailedData = @"
+SELECT PTS.UIN AS ID, V_FIRM.TLGR AS Фирма, PTS.UNTS AS [Учетный номер ТС],VIDTC.SHNAME AS [Вид ТС], TIPTR.TNAME AS [Тип ТС], PTS.GRP AS [Грузоподъёмность], PTS.NORMT AS [Расход топлива] 
+FROM (
+    (V_FIRM INNER JOIN PTS ON V_FIRM.FIRMID = PTS.FIRMID) 
+    INNER JOIN TIPTR ON TIPTR.TID = PTS.TID) 
+INNER JOIN VIDTC ON VIDTC.VIDT = TIPTR.VIDT 
+GROUP BY V_FIRM.TLGR, VIDTC.SHNAME, TIPTR.TNAME, PTS.UNTS, PTS.GRP, PTS.NORMT, PTS.UIN;
+";
+
+        public static string ReadDataByTLGR = @"
+SELECT V_FIRM.TLGR AS Фирма, VIDTC.SHNAME AS [Вид ТС], TIPTR.TNAME AS [Тип ТС], PTS.UNTS AS [Учетный номер ТС] 
+FROM(
+    (V_FIRM INNER JOIN PTS ON V_FIRM.FIRMID = PTS.FIRMID) 
+    INNER JOIN TIPTR ON TIPTR.TID = PTS.TID
+) 
+INNER JOIN VIDTC ON VIDTC.VIDT = TIPTR.VIDT 
+WHERE V_FIRM.TLGR = @TLGR
+GROUP BY V_FIRM.TLGR, VIDTC.SHNAME, TIPTR.TNAME, PTS.UNTS;
+";
+
+        public static string ReadDataBySHNAME = @"
+SELECT V_FIRM.TLGR AS Фирма, VIDTC.SHNAME AS [Вид ТС], TIPTR.TNAME AS [Тип ТС], PTS.UNTS AS [Учетный номер ТС]
+FROM(
+    (V_FIRM INNER JOIN PTS ON V_FIRM.FIRMID = PTS.FIRMID) 
+    INNER JOIN TIPTR ON TIPTR.TID = PTS.TID
+) 
+INNER JOIN VIDTC ON VIDTC.VIDT = TIPTR.VIDT 
+WHERE VIDTC.SHNAME = @SHNAME
+GROUP BY V_FIRM.TLGR, VIDTC.SHNAME, TIPTR.TNAME, PTS.UNTS;";
+
+        public static string ReadDataByTName = @"
+SELECT V_FIRM.TLGR AS Фирма, VIDTC.SHNAME AS [Вид ТС], TIPTR.TNAME AS [Тип ТС], PTS.UNTS AS [Учетный номер ТС]
+FROM(
+    (V_FIRM INNER JOIN PTS ON V_FIRM.FIRMID = PTS.FIRMID)
+    INNER JOIN TIPTR ON TIPTR.TID = PTS.TID
+)
+INNER JOIN VIDTC ON VIDTC.VIDT = TIPTR.VIDT 
+WHERE TIPTR.TNAME = @TNAME 
+GROUP BY V_FIRM.TLGR, VIDTC.SHNAME, TIPTR.TNAME, PTS.UNTS;
+";
+
+        public static string ReadDataByUNTS = @"
+SELECT V_FIRM.TLGR AS Фирма, PTS.UNTS AS [Учетный номер ТС],VIDTC.SHNAME AS [Вид ТС], TIPTR.TNAME AS [Тип ТС], PTS.GRP AS [Грузоподъёмность], PTS.NORMT AS [Расход топлива]
+FROM (
+    (V_FIRM INNER JOIN PTS ON V_FIRM.FIRMID = PTS.FIRMID)
+    INNER JOIN TIPTR ON TIPTR.TID = PTS.TID
+)
+INNER JOIN VIDTC ON VIDTC.VIDT = TIPTR.VIDT
+WHERE PTS.UNTS = @UNTS
+GROUP BY V_FIRM.TLGR, VIDTC.SHNAME, TIPTR.TNAME, PTS.UNTS, PTS.GRP, PTS.NORMT;
+";
+
+        public static string ReadTLGRs = "SELECT TLGR FROM V_FIRM;";
+
+        public static string ReadTNames = "SELECT TNAME FROM TIPTR;";
+
+        public static string FirmFilter = "SELECT FIRMID FROM V_FIRM WHERE TLGR = @TLGR;";
+
+        public static string TipTRFilter = "SELECT TID FROM TIPTR WHERE TNAME = @TNAME;";
+
+        public static string UpdatePTS = @"
+UPDATE PTS SET 
+    UNTS = COALESCE(@UNTS1, UNTS), 
+    TID = COALESCE(@TID, TID),
+    FIRMID = COALESCE(@FIRMID, FIRMID), 
+    GRP = COALESCE(@GRP, GRP),
+    NORMT = COALESCE(@NORMT, NORMT),
+WHERE UNTS = @UNTS2";
+    }
+
+    public class Query
+    {
+        OleDbConnection connection;
+        OleDbCommand command;
+        OleDbDataAdapter dataAdapter;
+
+        public Query(string Conn)
+        {
+            connection = new OleDbConnection(Conn);
+        }
+        public DataTable ReadData(OleDbCommand command)
+        {
+            var dt = new DataTable();
+            command.Connection.Open();
+            dataAdapter = new OleDbDataAdapter(command);
+            dt.Clear();
+            dataAdapter.Fill(dt);
+            command.Connection.Close();
+            return dt;
+        }
+
+        public DataTable ReadData() => ReadData(new OleDbCommand(SQL.ReadData, connection));
+
+        public DataTable ReadDetailedData() => ReadData(new OleDbCommand(SQL.ReadDetailedData, connection));
+
+        public DataTable ReadDataByTLGR(string tlgr)
+        {
+            var command = new OleDbCommand(SQL.ReadDataByTLGR, connection);
+            command.Parameters.Add("TLGR", tlgr);
+            return ReadData(command);
+        }
+
+        public DataTable ReadDataBySHNAME(string shname)
+        {
+            var command = new OleDbCommand(SQL.ReadDataBySHNAME, connection);
+            command.Parameters.Add("SHNAME", shname);
+            return ReadData(command);
+        }
+
+        public DataTable ReadDataByTNAME(string tname)
+        {
+            var command = new OleDbCommand(SQL.ReadDataByTName, connection);
+            command.Parameters.Add("TNAME", tname);
+            return ReadData(command);
+        }
+
+        public DataTable ReadDataByUNTS(string unts)
+        {
+            var command = new OleDbCommand(SQL.ReadDataByUNTS, connection);
+            command.Parameters.Add("UNTS", unts);
+            return ReadData(command);
+        }
+
+        public DataTable ReadTLGRs() => 
+            ReadData(new OleDbCommand(SQL.ReadTLGRs, connection));
+
+        public DataTable ReadTNames() => 
+            ReadData(new OleDbCommand(SQL.ReadTNames, connection));
+
+        public DataTable ReadFirm(string tlgr)
+        {
+            var command = new OleDbCommand(SQL.FirmFilter, connection);
+            command.Parameters.Add("TLGR", tlgr);
+            return ReadData(command);
+        }
+
+        public DataTable ReadTipTR(string tname)
+        {
+            var command = new OleDbCommand(SQL.TipTRFilter, connection);
+            command.Parameters.Add("TNAME", tname);
+            return ReadData(command);
+        }
+
+        public void UpdatePTS(string unts1, string tId, string firmId, 
+            string grp, string normt, string unts2)
+        {
+            connection.Open();
+            var command = new OleDbCommand(SQL.UpdatePTS, connection);
+            command.Parameters.Add("UNTS1", unts1);
+            command.Parameters.Add("TID", tId);
+            command.Parameters.Add("FIRMID", firmId);
+            command.Parameters.Add("GRP", grp);
+            command.Parameters.Add("NORMT", normt);
+            command.Parameters.Add("UNTS2", unts2);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void Add(string UNTS, string TID, string FIRMID, string GRP, string NORMT, string DATASP)
+        {
+            connection.Open();
+            command = new OleDbCommand($"INSERT INTO PTS(UNTS, TID, FIRMID, GRP, NORMT, DATASP) VALUES(@UNTS, @TID, @FIRMID, @GRP, @NORMT, @DATASP)", connection);
+            command.Parameters.AddWithValue("UNTS", UNTS);
+            command.Parameters.AddWithValue("TID", TID);
+            command.Parameters.AddWithValue("FIRMID", FIRMID);
+            command.Parameters.AddWithValue("GRP", GRP);
+            command.Parameters.AddWithValue("NORMT", NORMT);
+            command.Parameters.AddWithValue("DATASP", DATASP);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void DeletePTS(int UNTS)
+        {
+            connection.Open();
+            command = new OleDbCommand($"DELETE FROM PTS WHERE UNTS = @UNTS", connection);
+            command.Parameters.AddWithValue("UNTS", UNTS);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
--- Sample/Form3.cs
+++ Sample/Form3.cs
@@ -1,7 +1,5 @@
-﻿using Sample.Controller;
-using System;
-using System.Data;
-using System.Windows.Forms;
+﻿using DbLibrary.Controller;
+using Sample.Controller;
 
 namespace Sample
 {
@@ -15,12 +13,9 @@ namespace Sample
             controller = new Query(ConnectionString.ConnStr);
         }
 
-        DataTable table3;
-
         private void Form3_Load(object sender, EventArgs e)
         {
-            table3 = controller.Update($"SELECT PTS.UIN AS ID, V_FIRM.TLGR AS Фирма,  PTS.UNTS AS [Учетный номер ТС],VIDTC.SHNAME AS [Вид ТС], TIPTR.TNAME AS [Тип ТС], PTS.GRP AS [Грузоподъёмность], PTS.NORMT AS [Расход топлива] FROM ((V_FIRM INNER JOIN PTS ON V_FIRM.FIRMID = PTS.FIRMID) INNER JOIN TIPTR ON TIPTR.TID = PTS.TID) INNER JOIN VIDTC ON VIDTC.VIDT = TIPTR.VIDT GROUP BY V_FIRM.TLGR, VIDTC.SHNAME, TIPTR.TNAME, PTS.UNTS, PTS.GRP, PTS.NORMT, PTS.UIN;");
-            dataGridView1.DataSource = table3;
+            dataGridView1.DataSource = controller.ReadDetailedData();
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -28,75 +23,17 @@ namespace Sample
             this.Close();
         }
 
-        //UPDATE PTS SET ContactName = 'Alfred Schmidt', City = 'Frankfurt' WHERE UNTS = 1;
-
         private void Button2_Click(object sender, EventArgs e)
         {
-            controller.Edit(BuildUpdate());
-            //BuildUpdate();
-        }
-
-        private string BuildUpdate()
-        {
-            string update = "UPDATE PTS SET ";
-            string where = "WHERE UNTS = ";
-            string unts = "UNTS = ";
-            string tid = "TID = ";
-            string firmid = "FIRMID = ";
-            string grp = "GRP = ";
-            string normt = "NORMT = ";
-            string comma = ", ";
-            string result = update;
-            if (textBox3.Text != "")
-            {
-                result += unts;
-                result += textBox3.Text;
-                result += " ";
-            }
-            if (textBox2.Text != "")
-            {
-                if ((textBox2.Text != "") && (textBox3.Text != ""))
-                {
-                    result += comma;
-                }
-                result += tid;
-                result += textBox2.Text;
-                result += " ";
-            }
-            if (textBox1.Text != "")
-            {
-                if ((textBox1.Text != "") && ((textBox3.Text != "") || (textBox2.Text != "")))
-                {
-                    result += comma;
-                }
-                result += firmid;
-                result += textBox1.Text;
-                result += " ";
-            }
-            if (textBox4.Text != "")
-            {
-                if ((textBox4.Text != "") && ((textBox3.Text != "") || (textBox2.Text != "") || (textBox1.Text != "")))
-                {
-                    result += comma;
-                }
-                result += grp;
-                result += textBox4.Text;
-                result += " ";
-            }
-            if (textBox5.Text != "")
-            {
-                if ((textBox5.Text != "") && ((textBox3.Text != "") || (textBox2.Text != "") || (textBox1.Text != "") || (textBox4.Text != "")))
-                {
-                    result += comma;
-                }
-                result += normt;
-                result += textBox5.Text;
-                result += " ";
-            }
-            result += where;
-            result += (int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells["Учетный номер ТС"].Value.ToString())).ToString();
-
-            return result;
+            var unts = int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells["Учетный номер ТС"].Value.ToString()).ToString();
+            controller.UpdatePTS(
+                textBox3.NullIfEmpty(),
+                textBox2.NullIfEmpty(),
+                textBox1.NullIfEmpty(),
+                textBox4.NullIfEmpty(),
+                textBox5.NullIfEmpty(),
+                unts
+                );
         }
     }
 }
