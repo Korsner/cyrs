718c2b72d243fceea59c098d66aa399d803ddf42
Korsner <d.borodachev@mail.ru>
2022-10-30 09:18:04 +0200

    простой тест

---
+++ LbLibraryUnitTests/QueryTests.cs
@@ -0,0 +1,26 @@
+﻿using DbLibrary.Controller;
+using NUnit.Framework;
+
+namespace LbLibraryUnitTests
+{
+    [TestFixture]
+    public class QueryTests
+    {
+        private Query query;
+        [SetUp]
+        public void Init()
+        {
+            var connString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Test.mdb";
+            query = new Query(connString);
+        }
+
+
+
+        [Test]
+        public void ReadDataDoesntThrowException()
+        {
+            var data = query.ReadData();
+            Assert.IsNotNull(data);
+        }
+    }
+}
