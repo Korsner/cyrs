5bb5e80e1dab061b1ab7d52845523aa8c34a6670
Korsner <57417233+Korsner@users.noreply.github.com>
2022-10-31 00:05:58 +0700

    Модульные тесты (#11)

--- LbLibraryUnitTests/QueryTests.cs
+++ LbLibraryUnitTests/QueryTests.cs
@@ -22,10 +22,66 @@ namespace LbLibraryUnitTests
         }
 
         [Test]
-        public void AddWrongDataDoesntThrowException()
+        public void ReadDataReturnsCorrectNumberOfRows()
+        {
+            var data = query.ReadData();
+            Assert.AreEqual(23, data.Rows.Count);
+
+            data = query.ReadTLGRs();
+            Assert.AreEqual(2, data.Rows.Count);
+
+            data = query.ReadTNames();
+            Assert.AreEqual(8, data.Rows.Count);
+        }
+
+        [Test]
+        public void ReadFirmTest()
+        {
+            var data = query.ReadFirm("Деловые линии");
+            Assert.AreEqual(1, data.Rows.Count);
+            Assert.AreEqual(1, data.Columns.Count);
+
+            Assert.AreEqual("2", data.Rows[0].ItemArray[0].ToString());
+
+            data = query.ReadFirm("Зевс");
+            Assert.AreEqual(1, data.Rows.Count);
+            Assert.AreEqual(1, data.Columns.Count);
+
+            Assert.AreEqual("1", data.Rows[0].ItemArray[0].ToString());
+
+            data = query.ReadFirm("Юпитер");
+            Assert.AreEqual(0, data.Rows.Count);
+            Assert.AreEqual(1, data.Columns.Count);
+        }
+
+        [Test]
+        public void ReadTipTR()
+        {
+            var data = query.ReadTipTR("Автомобиль-тягач");
+            Assert.AreEqual(1, data.Rows.Count);
+            Assert.AreEqual(1, data.Columns.Count);
+
+            Assert.AreEqual("4", data.Rows[0].ItemArray[0].ToString());
+
+            data = query.ReadTipTR("Специальный прицеп");
+            Assert.AreEqual(1, data.Rows.Count);
+            Assert.AreEqual(1, data.Columns.Count);
+
+            Assert.AreEqual("7", data.Rows[0].ItemArray[0].ToString());
+
+            data = query.ReadTipTR("Автомобиль тягач");
+            Assert.AreEqual(0, data.Rows.Count);
+            Assert.AreEqual(1, data.Columns.Count);
+        }
+
+        [Test]
+        public void AddTests()
         {
             var c = query.Add("--##", null, "1", "2", "3", "4");
             Assert.AreEqual(-1, c);
+
+            c = query.Add("100", "2", "1", "2", "3", "10/10/2022");
+            Assert.AreEqual(1, c);
         }
 
         [Test]
