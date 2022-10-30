d75a3a0327480eb0f58df0c993a595fb82af4d6d
Korsner <d.borodachev@mail.ru>
2022-10-30 09:02:39 +0200

    добавлен проект для юнит-тестов

---
+++ LbLibraryUnitTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace LbLibraryUnitTests
+{
+    [TestFixture]
+    public class QueryTests
+    {
+        [Test]
+        public void IsNull()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
